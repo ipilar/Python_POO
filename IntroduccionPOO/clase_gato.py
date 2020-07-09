f35b424030b6466e810e31f25d35b2afbcaf86ad
ipilar <isabel.pilar.carrillo@gmail.com>
2020-07-09 03:20:07 +0000

    clase_gato.py

--- IntroduccionPOO/clase_gato.py
+++ IntroduccionPOO/clase_gato.py
@@ -27,3 +27,41 @@ miGato=Gato("Pelusa",1)
 
 #Imprimir un atributo
 print(miGato.nombre)
+
+#Cambiar un atributo e imprimir
+miGato.nombre="Pelusita"
+print(miGato.nombre)
+
+#Agregar un atributo de forma dinámica e imprimirlo
+miGato.raza="Siamés"
+print(miGato.raza)
+
+#Agregar elementos a una lista que es atributo de un objeto
+miGato.alimentos.append("pescado")
+miGato.alimentos.append("leche")
+print(miGato.alimentos)
+
+#Modificar la lista que es atributo de un objeto
+miGato.alimentos=["leche", "galletas"]
+print(miGato.alimentos)
+
+#Invocar un método de un objeto
+miGato.esAdulto()
+miGato.edad=2
+miGato.esAdulto()
+
+#Instanciar otro objeto de la misma clase
+otroGato=Gato("Cleo",3)
+
+#Guardar una lista en un atributo
+otroGato.alimentos=["pescado","pan"]
+
+#Invocar al mismo método en distintos objetos
+print(miGato.esAlimentoFavorito("leche"))
+print(otroGato.esAlimentoFavorito("leche"))
+
+#Acceder a atributo de clase de un objeto
+print(miGato.especie)
+
+#Acceder a atributo de clase sin instanciar objetos
+print(Gato.especie)
