267075dac9dbc0c1b0c65760e0a968e11f6d4c06
ipilar <isabel.pilar.carrillo@gmail.com>
2020-07-09 00:20:58 +0000

    Introduccion.py Definiciones de POO

---
+++ IntroduccionPOO/Introduccion.py
@@ -0,0 +1,30 @@
+"""
+POO con Python
+1.- Paradigma de Programación
+2.- Todo es un objeto
+3.- clases (Atributos o propiedades - Métodos)
+4.- objetos
+
+Objeto: una entidad que se quiere representar pero no es suficiente con una variable de los tipos mas básicos (int, float, bool,..)
+Por ejemeplo: un sistema de ventas podemos tener
+objetos como el vendedor, un articulo, una venta
+y objetos mas complejos y otros objetos pueden ser una ventana.
+
+Atributos: caracterisitcas propias que lo definen
+Métodos: acciones a realizar.
+
+CLASE
+"Molde" para obtener objetos con la misma
+estructura. Para obtener un objeto a partir de	una clase se le debe instanciar.
+
+Objetos de la misma clase: tienen los mismos
+metodos y atributos, pero los atributos pueden
+tener distintos valores o estados.
+
+
+class Gato:
+	pass
+
+mi_gato = Gato()
+
+"""
---
+++ PythonPOO/programa_04.py
@@ -0,0 +1,15 @@
+class Coche:
+
+	def __init__(self):
+		print("Constructor de Coche")
+
+	def acelerar(self):
+		print("Acelerar")
+
+	def frenar(self):
+		print("Frenar")
+
+objeto = Coche()
+objeto.acelerar()
+objeto.frenar()
+
