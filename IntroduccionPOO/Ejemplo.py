cba6ad0391c0109fe1adeb5bef4348057a605af5
ipilar <isabel.pilar.carrillo@gmail.com>
2020-07-09 02:40:41 +0000

    clase_gato.py

--- IntroduccionPOO/Ejemplo.py
+++ IntroduccionPOO/Ejemplo.py
@@ -1,11 +1,14 @@
 class Ejemplo:
-	
-    def __init__(self, parámetro1, parámetro2):
-        self.atributo1=parámetro1
-        self.atributo2=parámetro2
+	def __init__(self, parametro1, parametro2):
+		
+		self.atributo1 = parametro1
+		self.atributo2 = parametro2
 
 #instanciar un objeto pasando argumentos al constructor
 un_ejemplo=Ejemplo("un valor", "otro valor")
 
 #imprimir valor de un atributo
 print(un_ejemplo.atributo1)
+
+# imprimir otro valor de un Atributo
+print(un_ejemplo.atributo2)
---
+++ IntroduccionPOO/clase_gato.py
@@ -0,0 +1,29 @@
+###----------------------------------------
+###Ejemplo: implementación de la clase Gato
+###----------------------------------------
+
+class Gato:
+
+	#atributo de clase
+	especie = "mamifero"
+
+	def __init__(self, nombre, edad):
+		self.nombre=nombre
+		self.edad=edad
+		self.alimentos=[] 
+
+	def esAdulto(self):
+		if self.edad>1:
+			print(self.nombre, "es adulto")
+		else:
+			print(self.nombre, "es cachorro")
+
+	def esAlimentoFavorito(self,alimento):
+		return alimento in self.alimentos
+
+
+#Instanciar un objeto Gato   
+p=Gato("Pelusa",1)
+
+#Imprimir un atributo
+print(p.nombre)
